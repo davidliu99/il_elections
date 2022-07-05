42193db07a0a119c04eee7f5b8459c6ee05d3c59
davidliu99 <102577179+davidliu99@users.noreply.github.com>
2022-07-05 16:02:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Define useful constants.
 const blueCheck = `<svg xmlns="http://www.w3.org/2000/svg" style="height:0.9em; width:0.9em; margin: 0px 0.05em 0px 0.1em; vertical-align: -0.1em;" viewBox="0 0 24 24" enable-background="new 0 0 18 18"><path fill="#1276b7" d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"/></svg>`
 const redCheck = `<svg xmlns="http://www.w3.org/2000/svg" style="height:0.9em; width:0.9em; margin: 0px 0.05em 0px 0.1em; vertical-align: -0.1em;" viewBox="0 0 24 24" enable-background="new 0 0 18 18"><path fill="#c93136" d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"/></svg>`
+var regions = false;
 
 // Create the map object.
 var map = new maplibregl.Map({
@@ -107,6 +108,7 @@ const electionInfo = {
 }
 
 const fullName = {
+    "CB": ["Workers' Rights", "Illinois Right to Collective Bargaining referendum"],
     "Tax": ["Fair Tax", "Illinois Fair Tax referendum"],
     "Pres": ["President", "United States presidential election in Illinois"],
     "Sen": ["Senate", "United States Senate election in Illinois"],
@@ -115,6 +117,14 @@ const fullName = {
     "Treas": ["Treasurer", "Illinois Treasurer election"],
     "SoS": ["Sec. of State", "Illinois Secretary of State election"],
     "Comp": ["Comptroller", "Illinois Comptroller election"]
+}
+
+const regionNames = {
+    "chicago": "Chicago",
+    "suburbs": "Suburbs of Chicago",
+    "northern": "Northern Illinois",
+    "central": "Central Illinois",
+    "southern": "Southern Illinois"
 }
 
 // Create a popup, but don't add it to the map yet.
@@ -127,7 +137,7 @@ var popup = new maplibregl.Popup({
 // Event listener that displays the popup upon hover.
 function listener (e) {
     map.getCanvas().style.cursor = 'pointer';
-    var countyName = e.features[0].properties.name;
+    var areaName = regions ? regionNames[e.features[0].properties.NAME] : `${e.features[0].properties.name} County`;
     var demVotes = e.features[0].properties[`elections_${electionInfo[year][office][0]}${year}`];
     var repVotes = e.features[0].properties[`elections_${electionInfo[year][office][2]}${year}`];
     var totalVotes = e.features[0].properties[`elections_Total${office}${year}`];
@@ -148,8 +158,13 @@ function listener (e) {
     var repHTML = `<${repResult}-color style="background-color: #c93136;"></${repResult}-color> <${repResult}-name>${electionInfo[year][office][3]}</${repResult}-name> <${repResult}-votes>${repVotes.toLocaleString('en-US')}</${repResult}-votes> <${repResult}-pct>${repPct.toFixed(1)}%</${repResult}-pct>`
 
     popup.setLngLat({lng: e.lngLat.lng, lat: e.lngLat.lat + 0.01})
-        .setHTML(`<h4>${countyName} County</h4><info-grid> <header-name>Candidate</header-name> <header-votes>Votes</header-votes> <header-pct>Pct.</header-pct> ${repHTML} ${demHTML} </info-grid>`)
+        .setHTML(`<h4>${areaName}</h4><info-grid> <header-name>Candidate</header-name> <header-votes>Votes</header-votes> <header-pct>Pct.</header-pct> ${repHTML} ${demHTML} </info-grid>`)
         .addTo(map);
+};
+
+function handleMouseLeave () {
+    map.getCanvas().style.cursor = '';
+    popup.remove();
 };
 
 // Draw counties.
@@ -160,12 +175,18 @@ map.on('load', function () {
         'data':
         'https://raw.githubusercontent.com/davidliu99/il_elections/main/elections.geojson'
     });
+
+    map.addSource('regions', {
+        'type': 'geojson',
+        'data':
+        'https://raw.githubusercontent.com/davidliu99/il_elections/main/regions.geojson' 
+    })
     
     // Add a layer showing the state polygons.
     map.addLayer({
         'id': 'counties-layer',
         'type': 'fill',
-        'source': 'counties',
+        'source': regions ? 'regions' : 'counties',
         'paint': {
             'fill-color': [
                 'match', ['get', `elections_Winner${office}${year}`],
@@ -190,11 +211,17 @@ map.on('load', function () {
     // location of the click, with description HTML from its properties.
     map.on('mousemove', 'counties-layer', listener);
 
-    map.on('mouseleave', 'counties-layer', function() {
-        map.getCanvas().style.cursor = '';
-        popup.remove();
-    });
+    map.on('mouseleave', 'counties-layer', handleMouseLeave);
 });
+
+function formatMargin(margin) {
+    if (margin > 0) {
+        return `<span style="color:#1276b7">D+${(margin * 100).toFixed(1)}</span>`
+    }
+    else {
+        return `<span style="color:#c93136">R+${(margin * -100).toFixed(1)}</span>`
+    }
+}
 
 // Change the displayed election.
 function updateElection() {
@@ -215,7 +242,7 @@ function updateElection() {
         demResult = "win";
         repResult = "lose";
     }
-    else{
+    else {
         demResult = "lose";
         repResult = "win";
     }
@@ -230,13 +257,37 @@ function updateElection() {
     document.getElementById(`${repResult}-pct`).innerHTML = `<div>${(repTotal / voteTotal * 100).toFixed(1)}%</div>`;
     document.getElementById("total-votes").innerHTML = `<div>${voteTotal.toLocaleString('en-US')}</div>`
     
-    // Update map
+    // Update description.
+    if (year == "20" && office == "Tax") { // for the Fair Tax referendum only
+        document.getElementById("description").innerHTML = "The Illinois Fair Tax Amendment was rejected. Votes against the amendment outnumbered those for by a margin of <span style='color:#c93136;'>375,921 votes</span>, or <span style='color:#c93136; font-weight:500;'>6.5%</span>."
+    }
+    else {
+        var winner = (demTotal > repTotal) ? electionInfo[year][office][1] : electionInfo[year][office][3];
+        var winColor = (demTotal > repTotal) ? "#1276b7" : "#c93136";
+        document.getElementById("description").innerHTML = `${winner} won by a margin of <span style="color:${winColor};">${Math.abs(demTotal - repTotal).toLocaleString('en-US')} votes</span>, or <span style="color:${winColor}; font-weight:500;">${(Math.abs(demTotal - repTotal) / voteTotal * 100).toFixed(1)}%</span>.`;
+    }
+
+    // Update breakdown.
+    for (let i = 0; i < regionData.length; i++) {
+        let region = regionData[i]["NAME"];
+        let demVotes = regionData[i][`elections_${electionInfo[year][office][0]}${year}`];
+        let repVotes = regionData[i][`elections_${electionInfo[year][office][2]}${year}`];
+        let regionVotes = regionData[i][`elections_Total${office}${year}`];
+
+        document.getElementById(`${region}-margin`).innerHTML = formatMargin((demVotes - repVotes) / regionVotes);
+        document.getElementById(`${region}-share`).innerHTML = `${(regionVotes / voteTotal * 100).toFixed(1)}%`;
+    }
+
+    // Update map.
     map.removeLayer('counties-layer');
 
+    console.log(`elections_Winner${office}${year}`);
+    console.log(electionInfo[year][office][2]);
+    console.log(electionInfo[year][office][0])
     map.addLayer({
         'id': 'counties-layer',
         'type': 'fill',
-        'source': 'counties',
+        'source': regions ? 'regions' : 'counties',
         'paint': {
             'fill-color': [
                 'match', ['get', `elections_Winner${office}${year}`],
@@ -256,36 +307,9 @@ function updateElection() {
             ]
         }
     });
-
-    /* function listener (e) {
-        map.getCanvas().style.cursor = 'pointer';
-        var countyName = e.features[0].properties.name;
-        var demVotes = e.features[0].properties[`elections_${electionInfo[year][office][0]}${year}`];
-        var repVotes = e.features[0].properties[`elections_${electionInfo[year][office][2]}${year}`];
-        var totalVotes = e.features[0].properties[`elections_Total${office}${year}`];
-        var demPct = demVotes / totalVotes * 100;
-        var repPct = repVotes / totalVotes * 100;
-    
-        var demResult, repResult;
-        
-        if (demVotes > repVotes) {
-            demResult = "win"
-            repResult = "lose"
-        }
-        else {
-            demResult = "lose"
-            repResult = "win"
-        }
-        var demHTML = `<${demResult}-color style="background-color: #1276b7;"></${demResult}-color> <${demResult}-name>${electionInfo[year][office][1]}</${demResult}-name> <${demResult}-votes>${demVotes.toLocaleString('en-US')}</${demResult}-votes> <${demResult}-pct>${demPct.toFixed(1)}%</${demResult}-pct>`
-        var repHTML = `<${repResult}-color style="background-color: #c93136;"></${repResult}-color> <${repResult}-name>${electionInfo[year][office][3]}</${repResult}-name> <${repResult}-votes>${repVotes.toLocaleString('en-US')}</${repResult}-votes> <${repResult}-pct>${repPct.toFixed(1)}%</${repResult}-pct>`
-
-        popup.setLngLat({lng: e.lngLat.lng, lat: e.lngLat.lat + 0.01})
-            .setHTML(`<h4>${countyName} County</h4><info-grid> <header-name>Candidate</header-name> <header-votes>Votes</header-votes> <header-pct>Pct.</header-pct> ${repHTML} ${demHTML} </info-grid>`)
-            .addTo(map);
-    }; */
 }
 
-// Find the currently selected year.
+// Get the currently selected year.
 function getSelectedYear() {
     var choices = document.getElementsByName('year');
 
@@ -296,7 +320,7 @@ function getSelectedYear() {
     }
 }
 
-// Find the currently selected office.
+// Get the currently selected office.
 function getSelectedOffice() {
     var choices = document.getElementsByName('office');
 
@@ -338,4 +362,35 @@ function changeOffice() {
     office = getSelectedOffice();
 
     updateElection();
+}
+
+// Handle changes in mode selection.
+function viewCounties() {
+    if (regions) {
+        regions = false;
+        
+        updateElection();
+    }
+}
+
+function viewRegions() {
+    if (!regions) {
+        regions = true;
+
+        updateElection();
+    }
+}
+
+// Toggle breakdown table.
+function toggleBreakdown() {
+    let table = document.getElementById("breakdown");
+    let arrow = document.getElementById("arrow");
+    if (table.style.display == "none") {
+        table.style.display = "grid";
+        arrow.innerHTML = "▾";
+    }
+    else {
+        table.style.display = "none";
+        arrow.innerHTML = "▸";
+    }
 }
